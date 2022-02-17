205f2defa2b80d097647b87197463df644468d45
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2022-02-17 23:59:07 +0700

    update

--- JS/main.js
+++ JS/main.js
@@ -25,6 +25,9 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+
+
 /*Trending-Items*/
 let index = 0;
 const rightbtn = document.querySelector('.fa-chevron-right')
