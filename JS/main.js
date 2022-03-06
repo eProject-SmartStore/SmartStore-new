e9092bc63f3641c914361a20112f1925b47e3735
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2022-03-07 02:07:46 +0700

    update

--- JS/main.js
+++ JS/main.js
@@ -88,3 +88,16 @@ function removeOnes(j) {
     return nowOne[j].classList.remove('removeOne');
 }
 
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
