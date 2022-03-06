18ebacfa081953818dbe508f57e59c6c126757d3
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2022-03-07 02:28:04 +0700

    update

--- JS/main.js
+++ JS/main.js
@@ -38,7 +38,7 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 2000);
+    setTimeout(carousel, 3000);
 }
 
 /*Trending-Items*/
